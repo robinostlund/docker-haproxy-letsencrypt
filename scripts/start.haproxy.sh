717fdd86cc467a2d8979cafe7d025f4b40809b16
Robin Östlund <robin@kiisnet.se>
2018-01-04 15:32:04 +0100

    updates

--- scripts/start.haproxy.sh
+++ scripts/start.haproxy.sh
@@ -31,13 +31,11 @@ fi
 if [ ! -d /data/etc/haproxy ]; then
   mkdir -p /data/etc/haproxy
   chmod -R 0755 /data/etc/haproxy
-  chown -R haproxy:haproxy /data/etc/haproxy
 fi
 
 if [ ! -d /data/var/log/haproxy ]; then
   mkdir -p /data/var/log/haproxy
   chmod -R 0755 /data/var/log/haproxy
-  chown -R syslog:adm /data/var/log/haproxy
 fi
 
 if [ ! -d /run/haproxy ]; then
@@ -49,42 +47,39 @@ fi
 if [ ! -d /data/etc/haproxy/lua ]; then
   mkdir -p /data/etc/haproxy/lua
   chmod -R 0755 /data/etc/haproxy/lua
-  chown -R haproxy:haproxy /data/etc/haproxy/lua
 fi
 
 if [ ! -d /data/etc/haproxy/ssl ]; then
   mkdir -p /data/etc/haproxy/ssl
   chmod -R 0755 /data/etc/haproxy/ssl
-  chown -R haproxy:haproxy /data/etc/haproxy/ssl
 fi
 
 if [ ! -d /data/etc/haproxy/errors ]; then
   mkdir -p /data/etc/haproxy/errors
   chmod -R 0755 /data/etc/haproxy/errors
   cp -r /etc/haproxy/errors/* /data/etc/haproxy/errors/
-  chown -R haproxy:haproxy /data/etc/haproxy/errors
 fi
 
 # create files
 if [ ! -f /data/etc/haproxy/haproxy.cfg ]; then
   cp /root/haproxy/haproxy.cfg /data/etc/haproxy/haproxy.cfg
-  chown -R haproxy:haproxy /data/etc/haproxy/haproxy.cfg
 fi
 
 if [ ! -f /data/etc/haproxy/lua/acme-http01-webroot.lua ]; then
   cp /root/haproxy/lua/acme-http01-webroot.lua /data/etc/haproxy/lua/acme-http01-webroot.lua
-  chown -R haproxy:haproxy /data/etc/haproxy/lua/acme-http01-webroot.lua
 fi
 
 if [ ! -f /data/etc/haproxy/lua/robots_disallow.lua ]; then
   cp /root/haproxy/lua/robots_disallow.lua /data/etc/haproxy/lua/robots_disallow.lua
-  chown -R haproxy:haproxy /data/etc/haproxy/lua/robots_disallow.lua
 fi
 
 if [ ! -f /data/etc/haproxy/lua/healthcheck.lua ]; then
   cp /root/haproxy/lua/healthcheck.lua /data/etc/haproxy/lua/healthcheck.lua
-  chown -R haproxy:haproxy /data/etc/haproxy/lua/healthcheck.lua
 fi
+
+# set permissons
+chown -R haproxy:haproxy /data/etc/haproxy
+chown -R syslog:adm /data/var/log/haproxy
 
 # check if we have letsencrypt certificate
 if [ -f /data/etc/haproxy/ssl/haproxy.pem ]; then
